06be5695ae849693e66a429513eecaffb030e6c4
kri8or <kri8or@outlook.pt>
2015-03-31 11:02:07 -0400

    facebook authentication working in very beta mode

--- app.js
+++ app.js
@@ -78,23 +78,41 @@ passport.use(new passportLocal.Strategy(function(username, password, done){ //do
 // In order to use Facebook authentication, you must first create an app at https://developers.facebook.com/. 
 // When created, an app is assigned an App ID and App Secret. Your application must also implement a redirect URL, 
 // to which Facebook will redirect users after they have approved access for your application.
-
+var userTest = null;
 var FACEBOOK_APP_ID = 343195729218869;
 var FACEBOOK_APP_SECRET = 'd6e3830c44a08bc8b57032048d94dd65';
 
 passport.use(new passportFB({
 	clientID: FACEBOOK_APP_ID,
     clientSecret: FACEBOOK_APP_SECRET,
-    callbackURL: "http://www.example.com/auth/facebook/callback"
+    callbackURL: "https://quemsou.eu/fb/callback/"
 	},
   	function(accessToken, refreshToken, profile, done) {
 	
-	var userLogin = usersDB.loginUser(username,password); //null = insucesso
-	if (usersDB.loginUser(username,password)){
-		done(null, userLogin);
-	}else{
-		done(null,null);  //check http://passportjs.org/guide/configure/
-	}
+	//need to make the create/exists user Function...
+
+  // function(accessToken, refreshToken, profile, done) {
+  //               User.findOrCreate(
+  //                   { facebookId: profile.id },
+  //                   function (err, result) {
+  //                       if(result) {
+  //                           result.access_token = accessToken;
+  //                           result.save(function(err, doc) {
+  //                               done(err, doc);
+  //                           });
+  //                       } else {
+  //                           done(err, result);
+  //                       }
+  //                   }
+  //               );
+  //           }
+
+
+	//var userLogin = usersDB.loginUser(username,password); //null = insucesso
+	userTest = {'facebookId':profile.id, 'facebookProfile':profile.displayName }; // profile properties http://passportjs.org/guide/profile/
+
+	done(null,userTest);
+
 }));
 
 
@@ -103,12 +121,18 @@ passport.use(new passportFB({
 // ==============================================
 
 passport.serializeUser(function(user, done){
-	done(null,user.id);
+	done(null,user.facebookId);
 });
 
 passport.deserializeUser(function(id, done){
-	var user = usersDB.findUser(id);
-	done(null,{id: user.id, username: user.username});
+	// working
+	// var user = usersDB.findUser(id);
+	// done(null,{id: user.id, username: user.username});
+
+	//faking for fb
+	var user =userTest;
+	console.log('here--------------:'+user.facebookId+'  '+user.facebookProfile);  
+	done(null,{id: user.facebookId});
 });
 
 
--- routes/facebook.js
+++ routes/facebook.js
@@ -5,6 +5,15 @@ var fbRouter = express.Router();
 
 fbRouter.get('/facebook', passport.authenticate('facebook'));
 
+fbRouter.get('/callback',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+
+
+
 fbRouter.get('/', function (req, res) {
   res.render('index', {
   	isAuthenticated: req.isAuthenticated(), //passport adds this for us (express doesnt have)
