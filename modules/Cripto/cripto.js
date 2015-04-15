88225c4471f0544bb249a7c4e117a627a07d3614
kri8or <kri8or@outlook.pt>
2015-04-15 16:48:54 +0200

    register and login working fully with crypto

--- modules/Cripto/cripto.js
+++ modules/Cripto/cripto.js
@@ -8,10 +8,33 @@ exports.createHashedPassword = function (plainTextPassword, callback) {
     crypto.randomBytes(SALT_LENGTH, function (err, salt) {
         //console.time('password-hash');
         //crypto.pbkdf2(password, salt, iterations, keylen[, digest], callback)#
-        crypto.pbkdf2(plainTextPassword, salt, ITERATIONS, KEY_LENGTH, function (err, derivedKey) {
+        crypto.pbkdf2(plainTextPassword, salt.toString('base64'), ITERATIONS, KEY_LENGTH, function (err, derivedKey) {
         //console.timeEnd('password-hash');
             return callback(null, {derivedKey: derivedKey.toString('base64'), salt: salt.toString('base64'), iterations: ITERATIONS});
         });
+    });
+};
+
+
+
+//verify if login password is valid
+exports.verify = function (passwordEntered,storedPassword,storedSalt,callback){
+
+    crypto.pbkdf2(passwordEntered, storedSalt, ITERATIONS, KEY_LENGTH, function (err, derivedKey) {
+
+        //If no error....to be done...
+        var dKey =derivedKey.toString('base64');
+
+        //console.log('--dKey--'+dKey);
+        //console.log('--storedPassword--'+storedPassword);
+        //console.log('--storedSalt--'+storedSalt);
+        //console.log('--passwordEntered--'+passwordEntered);
+
+        if (dKey===storedPassword){
+            return callback(true) //sucess
+        }else{
+            return callback(false)
+        }
     });
 };
 
